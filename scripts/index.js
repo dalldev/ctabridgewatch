541753c80a04ca574a1d119ae0ca46eacf03081b
dalldev <dall.offic@gmail.com>
2021-12-02 21:32:26 -0600

    Hero image - mockup [beta]

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,11 @@
+window.onload = () => {
+  const $chargingScreen = document.getElementById('charging-screen');
+  const $body = document.body;
+
+  $chargingScreen.style.display = 'none';
+  $body.style.overflowY = 'scroll';
+};
+
 const $headerButtom = document.getElementById('headerButtom');
 
 $headerButtom.addEventListener('click', () => {
